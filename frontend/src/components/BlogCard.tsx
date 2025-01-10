6f5eceda35d26dc83a34da2b7bca66b8abe1578c
KrishanuSingh <krishanudavps@gmail.com>
2025-01-10 14:31:12 +0530

    changed avatar

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -50,8 +50,8 @@ export const BlogCard = ({
 }
 
 export function Avatar({name, size= "small"}: {name:string, size?:"small" |"big"}){
-    return <div className={`relative inline-flex items-center justify-center ${size === "small" ? "w-6 h-6" : "w-9 h-9"} overflow-hidden bg-gray-100 rounded-full dark:bg-gray-500`}>
-        <span className="font-medium text-gray-600 dark:text-gray-300">
+    return <div className={`relative inline-flex items-center justify-center ${size === "small" ? "w-6 h-6" : "w-9 h-9"} overflow-hidden bg-gray-100 rounded-full dark:bg-zinc-400`}>
+        <span className="font-medium text-white dark:text-white">
             {name[0]}
 
         </span>
